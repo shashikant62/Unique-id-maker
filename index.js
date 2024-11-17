8c6e651ed13a53d62906239356b8a7519cd7589a
shashikant Borude <119213408+shashikant62@users.noreply.github.com>
2024-11-18 03:33:51 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-class UniqueIdMaker {
-    static generate(length = 16) {
-        const timestamp = Date.now().toString(36); // Encode timestamp to base36
-        const randomBytes = crypto.randomBytes(Math.ceil(length / 2)).toString('hex'); // Generate random bytes
-        return `${timestamp}-${randomBytes.slice(0, length)}`; // Combine and limit the length
-    }
-}
-module.exports = UniqueIdMaker;
